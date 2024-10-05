dc95cbcff74bf88574da7687d14d85929962696b
Pallavi0605 <ingolepallavi06@gmail.com>
2024-10-05 12:15:58 +0530

    charges

--- src/pages/charges.jsx
+++ src/pages/charges.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import Header from "../components/common/Header";
+import StatCard from "../components/common/StatCard";
+import { Car, User } from 'lucide-react';
+import { motion } from 'framer-motion';
 
 function Charges() {
   const [charges, setCharges] = useState({
@@ -26,16 +29,28 @@ function Charges() {
 
     setTimeout(() => {
       setShowPopup(false);
-    }, 3000);  //3sec
+    }, 3000);
   };
 
   return (
-    <div className="bg-white flex-1 overflow-auto relative z-10  text-black">
+    <div className="bg-white flex-1 overflow-auto relative z-10 text-black">
       <Header title="Charges Management" />
+      <motion.div
+        className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-5 mb-12 mt-10 mx-3"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <StatCard name="1-10 KM Charge" icon={Car} value={charges.km_1_10} color="#6366F1" />
+        <StatCard name="10-20 KM Charge" icon={Car} value={charges.km_10_20} color="#8B5CF6" />
+        <StatCard name="20-30 KM Charge" icon={Car} value={charges.km_10_30} color="#8B5CF6" />
+        <StatCard name="Driver Referral" icon={User} value={charges.driverReferral} color="#EF4444" />
+        <StatCard name="Passenger Referral" icon={User} value={charges.passengerReferral} color="#EF4444" />
+      </motion.div>
 
       <form
         onSubmit={handleSubmit}
-        className="mt-9 space-y-6 bg-white p-6 rounded-lg shadow-2xl max-w-2xl mx-auto text-lg"
+        className="mt-9 space-y-6 bg-white p-6 rounded-lg shadow-2xl max-w-full mx-auto text-lg"
       >
         <h2 className="text-xl font-bold mb-6 text-center">Travel Fair</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
@@ -48,8 +63,8 @@ function Charges() {
               name="km_1_10"
               value={charges.km_1_10}
               onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-red-500"
-              placeholder="Enter charge for 1-10 KM"
+              className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-red-500"
+              placeholder="Enter charge "
               required
             />
           </div>
@@ -63,23 +78,23 @@ function Charges() {
               name="km_10_20"
               value={charges.km_10_20}
               onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-red-500"
-              placeholder="Enter charge for 10-20 KM"
+              className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-red-500"
+              placeholder="Enter charge "
               required
             />
           </div>
 
           <div>
             <label className="block text-gray-700 text-sm font-bold mb-2">
-              20-30 KM Charge
+              10-30 KM Charge
             </label>
             <input
               type="text"
               name="km_10_30"
               value={charges.km_10_30}
               onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-red-500"
-              placeholder="Enter charge for 20-30 KM"
+              className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-red-500"
+              placeholder="Enter charge "
               required
             />
           </div>
@@ -96,8 +111,8 @@ function Charges() {
               name="driverReferral"
               value={charges.driverReferral}
               onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-red-500"
-              placeholder="Enter amount for driver referral"
+              className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-red-500"
+              placeholder="Enter amount "
               required
             />
           </div>
@@ -111,8 +126,8 @@ function Charges() {
               name="passengerReferral"
               value={charges.passengerReferral}
               onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-red-500"
-              placeholder="Enter amount for passenger referral"
+              className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-red-500"
+              placeholder="Enter amount "
               required
             />
           </div>
@@ -128,7 +143,6 @@ function Charges() {
         </div>
       </form>
 
-      {/* Popup Notification */}
       {showPopup && (
         <div className="fixed top-4 right-4 p-4 bg-green-500 text-white rounded-lg shadow-lg transition-opacity duration-500 ease-in-out">
           Charges updated successfully!
